8e616f4585675ed5d47758ae7a6af88a668cf515
Guz-tech <gustavogs4040@gmail.com>
2024-08-22 00:59:03 +0000

    Commit  edit

---
+++ BalancoTrimestral.java
@@ -0,0 +1,9 @@
+public class BalancoTrimestral {
+    public static void main(String[] args) { 
+        int gastosjaneiro = 15000;
+        int gastosfevereiro = 23000;
+        int gastosmarco = 17000;
+        int resultado = gastosfevereiro + gastosjaneiro + gastosmarco;
+        System.out.println(resultado);
+    }
+}
